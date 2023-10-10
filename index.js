4533b4dfa0750a97653cd876824bed2626d390ac
studentdeveloperone <dhiego_ti@hotmail.com>
2023-10-10 16:57:38 -0300

    Example project configured with docker

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-const express = require('express');
-const config = require('./config/knexfile');
-
-const ApiRouter = require('./src/routes');
-
-const routes = new ApiRouter(config.development);
+import express from 'express';
+import pkg from 'pg';
+const { Client } = pkg;
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(express.json());
-app.use(routes.getRouter());
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    res.json({ API: 'OK' });
+});
+
+let client = null
+
+router.get('/test', async (req, res) => {
+    try {        
+        client = new Client({
+            host: 'postgres',
+            port: 5432,
+            database: 'mydatabase',
+            user: 'postgres',
+            password: 'postgres'
+        });
+
+        await client.connect();
+        
+        const results = await client.query('SELECT * FROM "user_test"');
+
+        res.json(results.rows);
+    } catch (error) {
+        console.error('Erro ao consultar o banco de dados:', error);
+        res.status(500).json({ error: 'Erro ao consultar o banco de dados' });
+    } finally {        
+        if (client) {
+            await client.end();
+        }
+    }
+});
+
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+    console.log(`App started on port ${port}`);
 });
