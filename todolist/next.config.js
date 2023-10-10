acee03ec03522cb1d8cd503e154531ea9e3bca20
studentdeveloperone <dhiego_ti@hotmail.com>
2023-10-10 20:48:32 -0300

    Example of project configured with docker for front

---
+++ todolist/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (config, _) => ({
+    ...config,
+    watchOptions: {
+      ...config.watchOptions,
+      poll: 800,
+      aggregateTimeout: 300,
+    },
+  }),
+}
+
+module.exports = nextConfig
